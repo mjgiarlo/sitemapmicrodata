7dc731d22122ccbb1814f3cd9d0f32c28e3285e5
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2013-03-25 18:29:57 -0700

    this time working

--- extract.rb
+++ extract.rb
@@ -5,11 +5,5 @@ require 'mida'
 sitemap = Nokogiri::HTML(open('http://d.lib.ncsu.edu/student-leaders/sitemap.xml'))
 sitemap.xpath('//loc').each do |loc|
   url = loc.content
-  url.gsub!('http', 'https')
-  puts url
-  begin
-    open(url) { |f| puts Mida::Document.new(f, url).items }
-  rescue OpenURI::HTTPError
-    next
-  end
+  open(url) { |f| puts Mida::Document.new(f, url).items }
 end
